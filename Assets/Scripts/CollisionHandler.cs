7bc0332a383328338622d4c11bdb3bc7c6dcb178
RossyRoo <rossesmusic@gmail.com>
2021-03-23 08:01:46 -0400

    Initial Commit

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //switch statements are conditionals that allow us to compare
+        //one var to several constants
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                Debug.Log("The level is over");
+                break;
+            case "Fuel":
+                Debug.Log("You picked up fuel.");
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    AudioSource audioSource;
+    [SerializeField] private float rocketThrust;
+    [SerializeField] private float rotationThrust;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * rocketThrust * Time.deltaTime);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        rb.AddRelativeForce(Vector3.left * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false;
+    }
+}
